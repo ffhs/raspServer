fe8f3ad3de06de8645acc98a7b74fb08356616a7
Thierry Baumann <thierry@swissmademarketing.com>
2015-12-03 07:00:23 +0100

    test

--- src/main/java/net/p45q/raspberryspring/Application.java
+++ src/main/java/net/p45q/raspberryspring/Application.java
@@ -1,11 +1,12 @@
 package net.p45q.raspberryspring;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
